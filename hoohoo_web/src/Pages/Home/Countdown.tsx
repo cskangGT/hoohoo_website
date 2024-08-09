85c44d7076d78d9de559304f63dc0939d33ad568
Jisan Park <gosbhs12@gmail.com>
2024-08-08 22:40:07 -0400

    Fix: redirect share feature

--- hoohoo_web/src/Pages/Home/Countdown.tsx
+++ hoohoo_web/src/Pages/Home/Countdown.tsx
@@ -237,9 +237,18 @@ const Countdown: React.FC = () => {
     };
     useEffect(() => {
       const hash = location.hash.replace('#', '');
-    if (hash) {
-      navigate(hash);
-    }
+      if (hash) {
+        const [page, query] = hash.split('#?');
+        const params = new URLSearchParams(query);
+        const link = params.get('link');
+        
+        if (link) {
+          console.log('Navigating to', page, 'with link', link);
+          navigate(`/${page}?link=${encodeURIComponent(link)}`);
+        } else {
+          navigate(`/${page}`);
+        }
+      }
       const timer = setInterval(() => {
         setTimeLeft(calculateTimeLeft());
       }, 1000);
