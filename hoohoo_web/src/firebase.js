600d4193845abbefae628931b7656fed8bdbd1d7
Jisan <gosbhs12@gmail.com>
2025-02-04 21:44:48 -0500

    Add: Google Analytics

---
+++ hoohoo_web/src/firebase.js
@@ -0,0 +1,22 @@
+import {getAnalytics} from 'firebase/analytics';
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAmFZIC5U_3XBiB7w9CShcTFFi-RzkbOrk',
+  authDomain: 'earthmeraofficialwebsite.firebaseapp.com',
+  projectId: 'earthmeraofficialwebsite',
+  storageBucket: 'earthmeraofficialwebsite.firebasestorage.app',
+  messagingSenderId: '71388009584',
+  appId: '1:71388009584:web:2b42513055377ba24210b5',
+  measurementId: 'G-RKL2JP6Y4G',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {analytics};
