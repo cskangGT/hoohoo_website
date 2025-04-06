6551f3a7f04eff35dbee0a757f3fad20eea5aaff
Jisan <gosbhs12@gmail.com>
2025-04-06 18:06:31 -0400

    Add: random images

--- zigu_web/src/Pages/Profile/components/WidgetItem.tsx
+++ zigu_web/src/Pages/Profile/components/WidgetItem.tsx
@@ -498,18 +498,19 @@ function WidgetItem({
       ecoActionCount: 675,
     },
     thumbnails: [
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
-      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300?random=1',
+      'https://picsum.photos/200/300?random=2',
+      'https://picsum.photos/200/300?random=3',
+      'https://picsum.photos/200/300?random=4',
+      'https://picsum.photos/200/300?random=5',
+      'https://picsum.photos/200/300?random=6',
+      'https://picsum.photos/200/300?random=7',
+      'https://picsum.photos/200/300?random=8',
     ],
   };
   const emWidgetData =
     hasEMWidgetType && isTemp ? initialEMWidgetData : widgetItem.widgetData;
+
   // if (isTemp) {
   //   return (
   //     <WidgetItemContainer
